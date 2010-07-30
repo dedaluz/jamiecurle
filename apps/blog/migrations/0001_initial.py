65ce654d13abf4dbac87790c6ec3913d37b88f0a
Jamie Curle <me@jamiecurle.com>
2010-07-30 07:54:18 +0100

    removed migration history for the blog, twas borkend

--- apps/blog/migrations/0001_initial.py
+++ apps/blog/migrations/0001_initial.py
@@ -15,12 +15,37 @@ class Migration(SchemaMigration):
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50, db_index=True)),
             ('description', self.gf('django.db.models.fields.TextField')()),
             ('content', self.gf('django.db.models.fields.TextField')()),
-            ('visible', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
+            ('status', self.gf('django.db.models.fields.SmallIntegerField')(default=2)),
             ('featured', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
             ('comments', self.gf('django.db.models.fields.BooleanField')(default=True, blank=True)),
             ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('tags', self.gf('tagging.fields.TagField')()),
         ))
         db.send_create_signal('blog', ['Post'])
+
+        # Adding M2M table for field stylesheets on 'Post'
+        db.create_table('blog_post_stylesheets', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('post', models.ForeignKey(orm['blog.post'], null=False)),
+            ('css', models.ForeignKey(orm['assets.css'], null=False))
+        ))
+        db.create_unique('blog_post_stylesheets', ['post_id', 'css_id'])
+
+        # Adding M2M table for field scripts on 'Post'
+        db.create_table('blog_post_scripts', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('post', models.ForeignKey(orm['blog.post'], null=False)),
+            ('js', models.ForeignKey(orm['assets.js'], null=False))
+        ))
+        db.create_unique('blog_post_scripts', ['post_id', 'js_id'])
+
+        # Adding M2M table for field images on 'Post'
+        db.create_table('blog_post_images', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('post', models.ForeignKey(orm['blog.post'], null=False)),
+            ('img', models.ForeignKey(orm['assets.img'], null=False))
+        ))
+        db.create_unique('blog_post_images', ['post_id', 'img_id'])
 
 
     def backwards(self, orm):
@@ -28,8 +53,37 @@ class Migration(SchemaMigration):
         # Deleting model 'Post'
         db.delete_table('blog_post')
 
+        # Removing M2M table for field stylesheets on 'Post'
+        db.delete_table('blog_post_stylesheets')
+
+        # Removing M2M table for field scripts on 'Post'
+        db.delete_table('blog_post_scripts')
+
+        # Removing M2M table for field images on 'Post'
+        db.delete_table('blog_post_images')
+
 
     models = {
+        'assets.css': {
+            'Meta': {'object_name': 'Css'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.FilePathField', [], {'path': "'/Users/jcurle/Sites/jamiecurle/templates/assets/css'", 'max_length': '100', 'recursive': 'True'})
+        },
+        'assets.img': {
+            'Meta': {'object_name': 'Img'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'license_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'src': ('apps.utils.fields.ImageWithThumbsField', [], {'blank': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'assets.js': {
+            'Meta': {'object_name': 'Js'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.FilePathField', [], {'path': "'/Users/jcurle/Sites/jamiecurle/templates/assets/js'", 'max_length': '100', 'recursive': 'True'})
+        },
         'blog.post': {
             'Meta': {'object_name': 'Post'},
             'comments': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
@@ -38,9 +92,13 @@ class Migration(SchemaMigration):
             'description': ('django.db.models.fields.TextField', [], {}),
             'featured': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'images': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['assets.Img']", 'null': 'True', 'blank': 'True'}),
+            'scripts': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['assets.Js']", 'null': 'True', 'blank': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'visible': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'})
+            'status': ('django.db.models.fields.SmallIntegerField', [], {'default': '2'}),
+            'stylesheets': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['assets.Css']", 'null': 'True', 'blank': 'True'}),
+            'tags': ('tagging.fields.TagField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         }
     }
 
