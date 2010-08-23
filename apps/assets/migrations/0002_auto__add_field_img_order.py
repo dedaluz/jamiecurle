f0bda559ed1c8a56eeb7a460f2ae1300f7dab6aa
Jamie Curle <me@jamiecurle.com>
2010-08-23 05:49:03 +0100

    thinking about the ordering of the assets, some work needs to be done

---
+++ apps/assets/migrations/0002_auto__add_field_img_order.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Img.order'
+        db.add_column('assets_img', 'order', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=5), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Img.order'
+        db.delete_column('assets_img', 'order')
+
+
+    models = {
+        'assets.css': {
+            'Meta': {'object_name': 'Css'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.FilePathField', [], {'path': "'/Users/jcurle/Sites/jamiecurle/templates/assets/css'", 'max_length': '100', 'recursive': 'True'})
+        },
+        'assets.img': {
+            'Meta': {'object_name': 'Img'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'license_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '5'}),
+            'src': ('apps.utils.fields.ImageWithThumbsField', [], {'blank': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'assets.js': {
+            'Meta': {'object_name': 'Js'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.FilePathField', [], {'path': "'/Users/jcurle/Sites/jamiecurle/templates/assets/js'", 'max_length': '100', 'recursive': 'True'})
+        }
+    }
+
+    complete_apps = ['assets']
