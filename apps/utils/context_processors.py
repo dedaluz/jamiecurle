1626477f7132f897369fb35c6a119ba09997745a
Jamie Curle <me@jamiecurle.com>
2010-09-28 06:44:04 +0100

    updated the style for ordered lists

--- apps/utils/context_processors.py
+++ apps/utils/context_processors.py
@@ -2,5 +2,6 @@ from django.conf import settings
 
 def grid(request):
     return{
+        'DISQUS_DEV' : getattr(settings, 'DISQUS_DEV', False),
         'GRID' : getattr(settings, 'GRID', False)
     }
