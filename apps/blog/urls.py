0ea001cd8a96ff8f7a70b53b9c63ba63b3664028
Jamie Curle <me@jamiecurle.com>
2010-09-29 06:31:11 +0100

    added rss to the blog

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from views import archive, month, post
+from feeds import LatestPostFeed
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^/(?P<year>[\d]{4})/(?P<month>[\d]{2})/(?P<slug>[-\w]+)\.html$', post, name='blog_post'),
     url(r'^/(?P<year>[\d]{4})/(?P<month>[\d]{2})\.html$', month, name='blog_month'),
     url(r'^\.html$', archive, name='blog'),
+    url(r'^\.rss$', LatestPostFeed(), name='blog_feed'),
 )
