0577ae77a5c55913e8332ff5d594b48c8deeb215
Jamie Curle <me@jamiecurle.com>
2010-10-27 08:04:52 +0100

    workout templates are working nicely now

--- apps/ultra/models.py
+++ apps/ultra/models.py
@@ -29,5 +29,23 @@ class SetTemplate(models.Model):
         return '%s x %s' % (self.reps, self.movement.name)
     
 
+
+class Workout(models.Model):
+    workouttemplate = models.ForeignKey(WorkoutTemplate)
+    notes = models.TextField(blank=True, null=True)
+    completed = models.DateField(default=datetime.now)
+    
+    def __unicode__(self):
+        return u'%s %s-%s' % (self.workouttemplate.name)
+    
+    
+class Set(models.Model):
+    movement = models.ForeignKey(Movement)
+    total = models.PositiveSmallIntegerField(blank=True, null=True)
+    reps = models.PositiveSmallIntegerField()
+    
+    def __unicode__(self):
+        return '%s x %s' % (self.reps, self.movement.name)
+    
 #class CompletedWorkout(models.Model):
     
--- apps/ultra/views.py
+++ apps/ultra/views.py
@@ -2,10 +2,35 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
+from forms import WorkoutForm, SetForm
+from models import Workout, Set, WorkoutTemplate
 
 def workout(request):
-
+    template = WorkoutTemplate.objects.get(pk=3)
+    
+    if request.method == 'POST':
+        workoutform = WorkoutForm(request.POST)
+        #setform = SetForm(request.POST)
+    if request.method == 'GET':
+        #
+        #
+        #
+        workoutform = WorkoutForm()
+        setforms = []
+        # set forms for each of the sets in the template
+        for i, wo_set in enumerate(template.settemplate_set.all()):
+            initial = {
+                'movement' : wo_set.movement.pk,
+                'total' : wo_set.total,
+                'reps' : wo_set.reps,
+            }
+            prefix = 'wo_set_%s' % i
+            # append the form to the wo_sets
+            setforms.append(SetForm(initial=initial, prefix=prefix))
+        #setform = SetForm()
     
     return render_to_response('ultra/workout.html', RequestContext(request, {
+        'workoutform' : workoutform,
+        'setforms' : setforms
         #'form' : form
     }))
