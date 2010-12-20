c79e98bd64bf50f47a29fec45d6877abdca463bc
Jamie Curle <me@jamiecurle.com>
2010-12-20 14:28:46 +0000

    Beasted.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jamiecurle::Application.config.secret_token = '2660455b9195eb3010ef959cc586995900c002ff4f690645f6511e579de1e055b4a1a6d40a0a078c8c7a06fa400704d8a4f4fa133066ca01cef0df50159c87d1'
+Jamiecurle::Application.config.secret_token = '6aed2f203a6a65cee581aa2617d03213e2ac0f06ba7ada299aa7c9fa9451f7ffa4a0b1b6c00f3888dbde0625dae3599031e08d895348a561f3e75851ddc316e8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Jamiecurle::Application.routes.draw do
-  resources :users
-
-  resources :posts
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
