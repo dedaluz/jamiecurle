6e7f614ff37a8bff238f411096a154811bd74706
Jamie Curle <me@jamiecurle.com>
2010-12-20 16:24:40 +0000

    ery bvasic blog is in with style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Jamiecurle::Application.routes.draw do
+  resources :users
+
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Jamiecurle::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "posts#index"
 
   # See how all your routes lay out with "rake routes"
 
