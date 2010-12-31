20f04c9e6f4bac4e103de73e5e70f555852b6035
Jamie Curle <me@jamiecurle.com>
2010-12-31 08:08:02 +0000

    Filenames are nice now, just have to make sure they're unique and that they delete ok.

--- app/models/blog_image.rb
+++ app/models/blog_image.rb
@@ -1,5 +1,4 @@
 class BlogImage < ActiveRecord::Base
-  
   belongs_to :post
   
   validates :src, :presence => true
@@ -10,41 +9,65 @@ class BlogImage < ActiveRecord::Base
   before_create :process_upload
   
   def process_upload
-    # give it a sanesible filename
+    # set up some sensiable filenames
     @filename = sanitize_filename(self.src.original_filename)
+    @src_title_filename = self.title.gsub(/[^\w\.\_]/,'_') + '.jpg'
     # Make the path if it doesn't exist
-    if !File.exists?(File.dirname(self.path))
-        FileUtils.mkdir_p(File.dirname(self.path))
+    if !File.exists?(File.dirname(self.original_path))
+        FileUtils.mkdir_p(File.dirname(self.original_path))
     end
-    # move it
-    FileUtils.copy(self.src.tempfile.path, self.path)
-    #
-    #
-    # todo make, sure names are unique
+    # move it and write attribute
+    FileUtils.copy(self.src.tempfile.path, self.original_path)
     write_attribute(:original, "/files/#{self.post.id}/#{@filename}")
     #
     #
-    # todo - offer a resize on the src
-    write_attribute(:src, "/files/#{self.post.id}/#{@filename}")
+    # src needs to be resized to 833 wide
+    src = MiniMagick::Image.open(self.original_path)
+    src.resize(833)
+    src.write(self.src_path)
+    write_attribute(:src, "/files/#{self.post.id}/#{@src_title_filename}")
+    #
+    #
     # create an 100 x 100 thumbnail
-    thumbnail = MiniMagick::Image.open(self.path)
-    thumbnail.resize "100x100"
+    thumbnail = MiniMagick::Image.open(self.original_path)
+    thumbnail = resize_and_crop(thumbnail, 200)
     thumbnail.write(self.thumb_path)
     write_attribute(:thumb, "/files/#{self.post.id}/thumb_#{@filename}")
   end
   
-  def path
+  
+  def original_path
       File.expand_path("#{RAILS_ROOT}/public/files/#{self.post.id}/#{@filename}")
   end
-  def thumb_path
-      File.expand_path("#{RAILS_ROOT}/public/files/#{self.post.id}/thumb_#{@filename}")
+
+  def src_path
+      File.expand_path("#{RAILS_ROOT}/public/files/#{self.post.id}/#{@src_title_filename}")
   end
+
+  def thumb_path
+      File.expand_path("#{RAILS_ROOT}/public/files/#{self.post.id}/thumb_#{@src_title_filename}")
+  end
+  
+  
+  def resize_and_crop(image, size)
+    if image[:width] < image[:height]
+      remove = ((image[:height] - image[:width])/2).round
+      image.shave("0x#{remove}")
+    elsif image[:width] > image[:height]
+      remove = ((image[:width] - image[:height])/2).round
+      image.shave("#{remove}x0")
+    end
+    image.resize("#{size}x#{size}")
+    return image
+  end
+
 
   private
     def sanitize_filename(original_filename)
       # get only the filename, not the whole path (from IE)
       filename = File.basename(original_filename) 
       # replace all none alphanumeric, underscore or perioids with underscore
-      filename.gsub(/[^\w\.\_]/,'_') 
+      filename.gsub(/[^\w\.\_]/,'_')
+      # todo make, sure names are unique, if not make them unique
     end
 end
--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -33,16 +33,16 @@
     */
     cursorPosition = 0;
     setCursorPosition = function(el, msg){
-      cursorPosition = textarea[0].selectionStart
+      cursorPosition = textarea[0].selectionStart;
     }
     getCursorPosition = function(){
-      return cursorPosition
+      return cursorPosition;
     }
     /*
       Inject image into textarea
     */
     writeImageMarkDown = function(event, xhr){
-      var r = window.editor_response
+      var r = window.editor_response;
       if(r.status == 'success'){
         var markdown = "![" + r.title + "](" + r.src + ")\n\n";
         var cursorPos = getCursorPosition();
@@ -57,7 +57,7 @@
     */
     $(this).each(function() {
       // set textarea
-      textarea = $(this)
+      textarea = $(this);
       // append ui
       $(this).before(img_ui);
       // set binds for click and keypress
@@ -66,123 +66,4 @@
   }
 })(jQuery);
 
-/*
-if (uploaded_image.status == 'success'){
-  image_path = "![" + uploaded_image.title + "](" + uploaded_image.src + ")\n\n";
-  // now insert the return 
-  //var before = textarea.val().substring(0, currentPosition);
-  //var after = textarea.val().substring(currentPosition, textarea.val().length);
-  //textarea.val(before + image_path + after);
-  // close fancybox
-  $.fancybox.close();
-}
-
-*/
-
-/*
-// cursorPosition is 0
-cursorPosition = 0;
-// set cursorPosition
-setCursorPosition = function(el){
-  // IE Support
-  if (document.selection) {
-      el.focus();
-      var Sel = document.selection.createRange();
-      var SelLength = document.selection.createRange().text.length;
-      Sel.moveStart('character', -el.value.length);
-      cursorPosition = Sel.text.length - SelLength;
-  }
-  // Firefox support
-  else if (el.selectionStart || el.selectionStart == '0'){
-      cursorPosition = el.selectionStart;
-  }
-}
-
-
-getCurrentPosition = function(){
-  console.log(currentPosition)
-  return currentPosition;
-}
-
-setCursorPosition = function() {
-    var pos = 0;
-    var el = $(this).get(0);
-    // IE Support
-    if (document.selection) {
-        el.focus();
-        var Sel = document.selection.createRange();
-        var SelLength = document.selection.createRange().text.length;
-        Sel.moveStart('character', -el.value.length);
-        pos = Sel.text.length - SelLength;
-    }
-    // Firefox support
-    else if (el.selectionStart || el.selectionStart == '0')
-        pos = el.selectionStart;
-
-    return pos;
-}
-
-
-jQuery.fn.editor = function(options){
-    // set up variables
-    var cursorPosition, img, image_path;
-    // create our "UI" elmeent
-    img = $('<a>', {'href':'#'}).click(function(event){
-    // cursorPosition is 0
-    cursorPosition = 0;
-    // set cursorPosition
-    setCursorPosition = function(el){
-      // IE Support
-      if (document.selection) {
-          el.focus();
-          var Sel = document.selection.createRange();
-          var SelLength = document.selection.createRange().text.length;
-          Sel.moveStart('character', -el.value.length);
-          cursorPosition = Sel.text.length - SelLength;
-      }
-      // Firefox support
-      else if (el.selectionStart || el.selectionStart == '0'){
-          cursorPosition = el.selectionStart;
-      }
-    }
-    
-    // load up the fancybox
-    $.fancybox({
-      href : '/posts/'+ $('#post_id').val() +'/blog_images/new',
-      width: 400,
-      height: 200,
-      autoDimensions : false,
-      onComplete: function(){
-        // bind a function to the remotipart return
-        $('#fancybox-content form').bind('ajax:complete',(function(event, xhr){
-          if (uploaded_image.status == 'success'){
-            image_path = "![" + uploaded_image.title + "](" + uploaded_image.src + ")\n\n";
-            // now insert the return 
-            //var before = textarea.val().substring(0, currentPosition);
-            //var after = textarea.val().substring(currentPosition, textarea.val().length);
-            //textarea.val(before + image_path + after);
-            // close fancybox
-            $.fancybox.close();
-          }
-        }))
-      }
-    });
-    // prevent the link from doing anything
-    event.preventDefault();
-  }).text('+ Image')
-  // now do the plugin init stuff
-  return this.each(function(){
-    // apend the minimal button
-    $(this).before(img);
-    // add events
-    $(this).bind('keypress', function(){
-      
-      console.log(jQuery.fn.editor)
-      //setCursorPosition($(this))
-    }).bind('click', function(){
-      //setCursorPosition($(this))
-    });
-  });
-}
-*/
 $('.editor textarea').editor();
