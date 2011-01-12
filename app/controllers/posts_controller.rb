cae3b4058b911cb8ad646d5cf1140a57305df32a
Jamie Curle <me@jamiecurle.com>
2011-01-12 13:43:11 +0000

    made a few small adjustments that make blogging easier

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -86,7 +86,8 @@ class PostsController < ApplicationController
     
     respond_to do |format|
       if @post.update_attributes(params[:post])
-        format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
+        format.html { redirect_to( edit_post_path, :notice => 'Post was successfully updated.' )}
+        #format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
