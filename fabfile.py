2e4fc1dc023deb8c5cd0f26c72fe495586edb63e
Jamie Curle <me@jamiecurle.com>
2011-04-24 10:55:28 +0100

    fixed compress errors

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,14 @@ from fabric.api import *
 env.hosts = ['jamiecurle.com']
 env.user = 'jcurle'
 
+def d():
+    run('cd sites/jamiecurle/jamiecurle/; git pull origin master')
+    run('supervisorctl restart jamiecurle')
+
+
 def deploy():
     local('git commit -a')
     local('git push origin master')
     run('cd sites/jamiecurle/jamiecurle/; git pull origin master')
-    run('supervisorctl restart reflection')
+    run('supervisorctl restart jamecurle')
 
