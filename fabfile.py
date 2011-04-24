cb501a02daaf24da7c0ee76f16c0b8c912bf87b7
Jamie Curle <me@jamiecurle.com>
2011-04-24 10:50:25 +0100

    added fabfile

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import *
+
+
+env.hosts = ['jamiecurle.com']
+env.user = 'jcurle'
+
+
+def deploy():
+    local('git commit -a')
+    local('git push origin master')
+    run('cd sites/jamiecurle/jamiecurle/; git pull origin master')
+    run('supervisorctl restart reflection')
+
