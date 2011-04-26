d9f11542323f4379a4e7d1eca27b42ac298644ae
Jamie Curle <me@jamiecurle.com>
2011-04-26 05:44:09 +0100

    Tag search url was in the wrong order

--- apps/tags/urls.py
+++ apps/tags/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from views import tag_search, tag_list
 
 urlpatterns = patterns('',
-    url(r'^(?P<tag>[\w\-]+)/$', tag_list, name="tag_list"),
     (r'^search/$', tag_search ),
+    url(r'^(?P<tag>[\w\-]+)/$', tag_list, name="tag_list"),
 )
 
