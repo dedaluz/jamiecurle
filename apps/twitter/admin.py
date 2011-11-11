9bb8782666e9d08793cbd4e23903670dffe7c3d2
Jamie Curle <me@jamiecurle.com>
2011-11-11 13:05:35 +0000

    now with twitter search

--- apps/twitter/admin.py
+++ apps/twitter/admin.py
@@ -3,5 +3,6 @@ from models import Tweet
 
 class TweetAdmin(admin.ModelAdmin):
     list_display = ['body', 'twitter_id', 'created']
+    search_fields = ['body']
 
 admin.site.register(Tweet, TweetAdmin)
