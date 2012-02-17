b20f175b2af7e5ed56665a8a872f8dfdecccd38a
Jamie Curle <me@jamiecurle.com>
2012-02-17 10:43:26 +0000

    added cache manefest

--- jamiecurle/config.py
+++ jamiecurle/config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 DEBUG = False
+MANIFEST_ENABLED = True
 CACHE_ENABLED = True
 BLOG_CONTENT_PATH = '/home/curle/sites/jamiecurle/jamiecurle/jamiecurle/content/blog'
 MEMCACHE_CONNECTION = 'unix:/home/curle/tmp/memcached.sock'
