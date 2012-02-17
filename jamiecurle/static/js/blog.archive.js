46014cbe661abd1235651e77f6a6c8e95b024566
Jamie Curle <me@jamiecurle.com>
2012-02-17 11:51:39 +0000

    removed manifest

--- jamiecurle/static/js/blog.archive.js
+++ jamiecurle/static/js/blog.archive.js
@@ -1,34 +1,37 @@
-$('div.secondary nav').each(function(){
-	//var nav = $(this);
-	var queue = []
-	$(this).find('.stale').each(function(){
-		queue.push($(this));
-		$(this).css({display: 'block'}).hide();
-	})
+$(function(){
+	$('div.secondary nav').each(function(){
+		//var nav = $(this);
+		var queue = []
+		$(this).find('.stale').each(function(){
+			queue.push($(this));
+			$(this).css({display: 'block'}).hide();
+		})
 
-	var toggler = $('<a>')
-				.addClass('toggler')
-				.css({display: 'block'})
-				.toggle(show, hide)
-				.text('Show more');
+		var toggler = $('<a>')
+					.addClass('toggler')
+					.css({display: 'block'})
+					.toggle(show, hide)
+					.text('Show more');
 
 
-	function show(){
-		var show_queue = queue.slice(0);
-		while(show_queue.length > 0){
-			var el = show_queue.pop();
-			el.slideDown(300);
+		function show(){
+			var show_queue = queue.slice(0);
+			while(show_queue.length > 0){
+				var el = show_queue.pop();
+				el.slideDown(300);
+			}
+			$(this).text('Show less');
 		}
-		$(this).text('Show less');
-	}
 
-	function hide(){
-		var hide_queue = queue.slice(0);
-		while(hide_queue.length > 0){
-			var el = hide_queue.pop();
-			el.css({display: 'block'}).slideUp(200);
+		function hide(){
+			var hide_queue = queue.slice(0);
+			while(hide_queue.length > 0){
+				var el = hide_queue.pop();
+				el.css({display: 'block'}).slideUp(200);
+			}
+			$(this).text('Show more');
 		}
-		$(this).text('Show more');
-	}
-	$(this).find('a.stale:last').after(toggler);
-});
+		$(this).find('a.stale:last').after(toggler);
+	});
+	
+})
